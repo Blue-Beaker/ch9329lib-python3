95b96a0fa6c11f23b7fb72cf203ea6cf33010112
blue-beaker <yyy5566778899@126.com>
2023-01-05 11:41:24 +0800

    fixed various crashes

--- ch9329lib.py
+++ ch9329lib.py
@@ -9,213 +9,6 @@ import math
 import binascii
 import threading
 from socket import *
-#Constants
-__DICT_KEY_NORMAL=dict({
-    "esc":0x29,#Esc
-    "f1":0x3a,#F1
-    "f2":0x3b,#F2
-    "f3":0x3c,#F3
-    "f4":0x3d,#F4
-    "f5":0x3e,#F5
-    "f6":0x3f,#F6
-    "f7":0x40,#F7
-    "f8":0x41,#F8
-    "f9":0x42,#F9
-    "f10":0x43,#F10
-    "f11":0x44,#F11
-    "f12":0x45,#F12
-    "printscreen":0x46,#Print
-    "scrolllock":0x47,#ScrollLock
-    "pausebreak":0x48,#PauseBreak
-    "`":0x35,#`
-    "1":0x1e,#1
-    "2":0x1f,#2
-    "3":0x20,#3
-    "4":0x21,#4
-    "5":0x22,#5
-    "6":0x23,#6
-    "7":0x24,#7
-    "8":0x25,#8
-    "9":0x26,#9
-    "0":0x27,#0
-    "-":0x2d,#-
-    "=":0x2e,#=
-    "~":0x35,#~
-    "!":0x1e,#!
-    "@":0x1f,#@
-    "#":0x20,##
-    "$":0x21,#$
-    "%":0x22,#%
-    "^":0x23,#^
-    "&":0x24,#&
-    "*":0x25,#*
-    "(":0x26,#(
-    ")":0x27,#)
-    "_":0x2d,#_
-    "+":0x2e,#+
-    "backspace":0x2a,#Backspace
-    "tab":0x2b,#Tab
-    "q":0x14,#q
-    "w":0x1a,#w
-    "e":0x08,#e
-    "r":0x15,#r
-    "t":0x17,#t
-    "y":0x1c,#y
-    "u":0x18,#u
-    "i":0x0c,#i
-    "o":0x12,#o
-    "p":0x13,#p
-    "[":0x2f,#[
-    "]":0x30,#]
-    "\\":0x31,#\
-    "Q":0x14,#Q
-    "W":0x1a,#W
-    "E":0x08,#E
-    "R":0x15,#R
-    "T":0x17,#T
-    "Y":0x1c,#Y
-    "U":0x18,#U
-    "I":0x0c,#I
-    "O":0x12,#O
-    "P":0x13,#P
-    "{":0x2f,#{
-    "}":0x30,#}
-    "|":0x31,#|
-    "caps":0x39,#Caps
-    "a":0x04,#a
-    "s":0x16,#s
-    "d":0x07,#d
-    "f":0x09,#f
-    "g":0x0a,#g
-    "h":0x0b,#h
-    "j":0x0d,#j
-    "k":0x0e,#k
-    "l":0x0f,#l
-    ";":0x33,#;
-    "'":0x34,#'
-    "A":0x04,#A
-    "S":0x16,#S
-    "D":0x07,#D
-    "F":0x09,#F
-    "G":0x0a,#G
-    "H":0x0b,#H
-    "J":0x0d,#J
-    "K":0x0e,#K
-    "L":0x0f,#L
-    ":":0x33,#:
-    "\"":0x34,#"
-    "enter":0x28,#Enter
-    "z":0x1d,#z
-    "x":0x1b,#x
-    "c":0x06,#c
-    "v":0x19,#v
-    "b":0x05,#b
-    "n":0x11,#n
-    "m":0x10,#m
-    ",":0x36,#,
-    ".":0x37,#.
-    "/":0x38,#/
-    "Z":0x1d,#Z
-    "X":0x1b,#X
-    "C":0x06,#C
-    "V":0x19,#V
-    "B":0x05,#B
-    "N":0x11,#N
-    "M":0x10,#M
-    "<":0x36,#<
-    ">":0x37,#>
-    "?":0x38,#?
-    "space":0x2c,#Space
-    "app":0x65,#App
-    "menu":0x65,#Menu
-    "insert":0x49,#Insert
-    "delete":0x4c,#Delete
-    "home":0x4a,#Home
-    "end":0x4d,#End
-    "pageup":0x4b,#PageUp
-    "pagedown":0x4e,#PageDown
-    "left":0x50,#Left
-    "up":0x52,#Up
-    "right":0x4f,#Right
-    "down":0x51,#Down
-
-    })
-__DICT_KEY_CONTROL=dict({
-    "lctrl":7,#LCTRL
-    "lshift":6,#LSHIFT
-    "lalt":5,#LALT
-    "lmeta":5,#LMETA
-    "lsuper":4,#LSUPER
-    "lwin":4,#LWIN
-    "rctrl":3,#RCTRL
-    "rshift":2,#RSHIFT
-    "ralt":1,#RALT
-    "rmeta":1,#RMETA
-    "rsuper":0,#RSUPER
-    "rwin":0,#RWIN
-    })
-__DICT_KEY_MEDIA=dict({
-    "volup":[0,7],
-    "voldown":[0,6],
-    "mute":[0,5],
-    "playpause":[0,4],
-    "nexttrack":[0,3],
-    "prevtrack":[0,2],
-    "cdstop":[0,1],
-    "eject":[0,0],
-    "email":[1,7],
-    "search":[1,6],
-    "wwwfavorite":[1,5],
-    "wwwhome":[1,4],
-    "wwwback":[1,3],
-    "wwwforward":[1,2],
-    "wwwstop":[1,1],
-    "refresh":[1,0],
-    "media":[2,7],
-    "explorer":[2,6],
-    "calculator":[2,5],
-    "screensave":[2,4],
-    "mycomputer":[2,3],
-    "minimize":[2,2],
-    "record":[2,1],
-    "rewind":[2,0],
-})
-__NAMES_KEY_CONTROL=[
-    "rsuper",#RSUPER/RWIN
-    "ralt",#RALT
-    "rshift",#RSHIFT
-    "rctrl",#RCTRL
-    "lsuper",#LSUPER/LWIN
-    "lalt",#LALT
-    "lshift",#LSHIFT
-    "lctrl",#LCTRL
-    ]
-__NAMES_KEY_MEDIA=[
-    ["eject",
-    "cdstop",
-    "prevtrack",
-    "nexttrack",
-    "playpause",
-    "mute",
-    "voldown",
-    "volup",],
-    ["refresh",
-    "wwwstop",
-    "wwwforward",
-    "wwwback",
-    "wwwhome",
-    "wwwfavorite",
-    "search",
-    "email",],
-    ["rewind",
-    "record",
-    "minimize",
-    "mycomputer",
-    "screensave",
-    "calculator",
-    "explorer",
-    "media",]
-]
 
 class CH9329HID:
     #Parameters
@@ -224,6 +17,214 @@ class CH9329HID:
     path="/dev/ttyUSB0"
     baud=9600
     address=0x00
+    #Constants
+    __DICT_KEY_NORMAL=dict({
+        "esc":0x29,#Esc
+        "f1":0x3a,#F1
+        "f2":0x3b,#F2
+        "f3":0x3c,#F3
+        "f4":0x3d,#F4
+        "f5":0x3e,#F5
+        "f6":0x3f,#F6
+        "f7":0x40,#F7
+        "f8":0x41,#F8
+        "f9":0x42,#F9
+        "f10":0x43,#F10
+        "f11":0x44,#F11
+        "f12":0x45,#F12
+        "printscreen":0x46,#Print
+        "scrolllock":0x47,#ScrollLock
+        "pausebreak":0x48,#PauseBreak
+        "`":0x35,#`
+        "1":0x1e,#1
+        "2":0x1f,#2
+        "3":0x20,#3
+        "4":0x21,#4
+        "5":0x22,#5
+        "6":0x23,#6
+        "7":0x24,#7
+        "8":0x25,#8
+        "9":0x26,#9
+        "0":0x27,#0
+        "-":0x2d,#-
+        "=":0x2e,#=
+        "~":0x35,#~
+        "!":0x1e,#!
+        "@":0x1f,#@
+        "#":0x20,##
+        "$":0x21,#$
+        "%":0x22,#%
+        "^":0x23,#^
+        "&":0x24,#&
+        "*":0x25,#*
+        "(":0x26,#(
+        ")":0x27,#)
+        "_":0x2d,#_
+        "+":0x2e,#+
+        "backspace":0x2a,#Backspace
+        "tab":0x2b,#Tab
+        "q":0x14,#q
+        "w":0x1a,#w
+        "e":0x08,#e
+        "r":0x15,#r
+        "t":0x17,#t
+        "y":0x1c,#y
+        "u":0x18,#u
+        "i":0x0c,#i
+        "o":0x12,#o
+        "p":0x13,#p
+        "[":0x2f,#[
+        "]":0x30,#]
+        "\\":0x31,#\
+        "Q":0x14,#Q
+        "W":0x1a,#W
+        "E":0x08,#E
+        "R":0x15,#R
+        "T":0x17,#T
+        "Y":0x1c,#Y
+        "U":0x18,#U
+        "I":0x0c,#I
+        "O":0x12,#O
+        "P":0x13,#P
+        "{":0x2f,#{
+        "}":0x30,#}
+        "|":0x31,#|
+        "caps":0x39,#Caps
+        "a":0x04,#a
+        "s":0x16,#s
+        "d":0x07,#d
+        "f":0x09,#f
+        "g":0x0a,#g
+        "h":0x0b,#h
+        "j":0x0d,#j
+        "k":0x0e,#k
+        "l":0x0f,#l
+        ";":0x33,#;
+        "'":0x34,#'
+        "A":0x04,#A
+        "S":0x16,#S
+        "D":0x07,#D
+        "F":0x09,#F
+        "G":0x0a,#G
+        "H":0x0b,#H
+        "J":0x0d,#J
+        "K":0x0e,#K
+        "L":0x0f,#L
+        ":":0x33,#:
+        "\"":0x34,#"
+        "enter":0x28,#Enter
+        "z":0x1d,#z
+        "x":0x1b,#x
+        "c":0x06,#c
+        "v":0x19,#v
+        "b":0x05,#b
+        "n":0x11,#n
+        "m":0x10,#m
+        ",":0x36,#,
+        ".":0x37,#.
+        "/":0x38,#/
+        "Z":0x1d,#Z
+        "X":0x1b,#X
+        "C":0x06,#C
+        "V":0x19,#V
+        "B":0x05,#B
+        "N":0x11,#N
+        "M":0x10,#M
+        "<":0x36,#<
+        ">":0x37,#>
+        "?":0x38,#?
+        "space":0x2c,#Space
+        "app":0x65,#App
+        "menu":0x65,#Menu
+        "insert":0x49,#Insert
+        "delete":0x4c,#Delete
+        "home":0x4a,#Home
+        "end":0x4d,#End
+        "pageup":0x4b,#PageUp
+        "pagedown":0x4e,#PageDown
+        "left":0x50,#Left
+        "up":0x52,#Up
+        "right":0x4f,#Right
+        "down":0x51,#Down
+
+        })
+    __DICT_KEY_CONTROL=dict({
+        "lctrl":7,#LCTRL
+        "lshift":6,#LSHIFT
+        "lalt":5,#LALT
+        "lmeta":5,#LMETA
+        "lsuper":4,#LSUPER
+        "lwin":4,#LWIN
+        "rctrl":3,#RCTRL
+        "rshift":2,#RSHIFT
+        "ralt":1,#RALT
+        "rmeta":1,#RMETA
+        "rsuper":0,#RSUPER
+        "rwin":0,#RWIN
+        })
+    __DICT_KEY_MEDIA=dict({
+        "volup":[0,7],
+        "voldown":[0,6],
+        "mute":[0,5],
+        "playpause":[0,4],
+        "nexttrack":[0,3],
+        "prevtrack":[0,2],
+        "cdstop":[0,1],
+        "eject":[0,0],
+        "email":[1,7],
+        "search":[1,6],
+        "wwwfavorite":[1,5],
+        "wwwhome":[1,4],
+        "wwwback":[1,3],
+        "wwwforward":[1,2],
+        "wwwstop":[1,1],
+        "refresh":[1,0],
+        "media":[2,7],
+        "explorer":[2,6],
+        "calculator":[2,5],
+        "screensave":[2,4],
+        "mycomputer":[2,3],
+        "minimize":[2,2],
+        "record":[2,1],
+        "rewind":[2,0],
+    })
+    __NAMES_KEY_CONTROL=[
+        "rsuper",#RSUPER/RWIN
+        "ralt",#RALT
+        "rshift",#RSHIFT
+        "rctrl",#RCTRL
+        "lsuper",#LSUPER/LWIN
+        "lalt",#LALT
+        "lshift",#LSHIFT
+        "lctrl",#LCTRL
+        ]
+    __NAMES_KEY_MEDIA=[
+        ["eject",
+        "cdstop",
+        "prevtrack",
+        "nexttrack",
+        "playpause",
+        "mute",
+        "voldown",
+        "volup",],
+        ["refresh",
+        "wwwstop",
+        "wwwforward",
+        "wwwback",
+        "wwwhome",
+        "wwwfavorite",
+        "search",
+        "email",],
+        ["rewind",
+        "record",
+        "minimize",
+        "mycomputer",
+        "screensave",
+        "calculator",
+        "explorer",
+        "media",]
+    ]
+
     #Internal variables
     __port=None
     __pressedKeysCont=[0,0,0,0,0,0,0,0]
@@ -301,7 +302,9 @@ class CH9329HID:
                 lock="{:0>8b}".format(data[2])
                 info=f"Ver:{hex(ver)},USB:{usb},Num{lock[-1]} Caps{lock[-2]} Scroll{lock[-3]}"
                 print(info)
-        return packet
+            return packet
+        else:
+            return False
     def write9329(self,cmd,data):
         try:
             packet=bytearray()
@@ -336,7 +339,7 @@ class CH9329HID:
                 pathsplit=self.path.split(":")
                 if self.debug:
                     print(f"connecting {pathsplit[0]}:{pathsplit[1] if pathsplit.__len__()>1 else 23}")
-                self.__port.connect((pathsplit[0], pathsplit[1] if pathsplit.__len__()>1 else 23))
+                self.__port.connect((pathsplit[0], int(pathsplit[1]) if pathsplit.__len__()>1 else 23))
             else:
                 self.__port=serial.Serial(port=self.path,baudrate=self.baud)
                 self.__port.timeout=2
@@ -362,7 +365,7 @@ class CH9329HID:
         self.write9329(cmd=0x02,data=packet)
         return self.write9329(0x01,bytearray())
     def pressNormal(self,hidcode,press=0):
-        if press:
+        if press==1:
             if not hidcode in self.__pressedKeysNormal:
                 if len(self.__pressedKeysNormal)<=6:
                     self.__pressedKeysNormal.append(hidcode)
@@ -370,6 +373,11 @@ class CH9329HID:
                     return True
                 else:
                     return False
+        elif press==-1:
+            if hidcode in self.__pressedKeysNormal:
+                self.pressNormal(hidcode,0)
+            else:
+                self.pressNormal(hidcode,1)
         else:
             try:
                 self.__pressedKeysNormal.remove(hidcode)
@@ -392,12 +400,12 @@ class CH9329HID:
         self.write9329(0x01,bytearray())
         return True
     def pressByName(self,name,press=0):
-        if name in __DICT_KEY_NORMAL:
-            return self.pressNormal(__DICT_KEY_NORMAL[name],press)
-        elif name in __DICT_KEY_CONTROL:
-            return self.pressControl(__DICT_KEY_CONTROL[name],press)
-        elif name in __DICT_KEY_MEDIA:
-            return self.pressMedia(__DICT_KEY_MEDIA[name][0],__DICT_KEY_MEDIA[name][1],press)
+        if name in self.__DICT_KEY_NORMAL:
+            return self.pressNormal(self.__DICT_KEY_NORMAL[name],press)
+        elif name in self.__DICT_KEY_CONTROL:
+            return self.pressControl(self.__DICT_KEY_CONTROL[name],press)
+        elif name in self.__DICT_KEY_MEDIA:
+            return self.pressMedia(self.__DICT_KEY_MEDIA[name][0],self.__DICT_KEY_MEDIA[name][1],press)
         else:
             return False
     def clickByName(self,name):
@@ -423,7 +431,7 @@ class CH9329HID:
         packet.append(y%128)
         packet.append(math.floor(y/128))
         packet.append(wheel)
-        self.write9329(0x04,packet)
+        return self.write9329(0x04,packet)
     def mouseRel(self,x,y,wheel=0):
         packet=bytearray()
         packet.append(0x01)
@@ -439,7 +447,7 @@ class CH9329HID:
         packet.append(cx)
         packet.append(cy)
         packet.append(wheel)
-        self.write9329(0x05,packet)
+        return self.write9329(0x05,packet)
     def mousePressClick(self,button=0,press=1): #Press=0: release, Press=1: press, Press=2: click, Press=-1: toggle, 
         if press==2:
             self.setMousePress(button,1)
@@ -459,23 +467,23 @@ class CH9329HID:
     def getPressedKeyNormal(self):
         pressed=[]
         for key in self.__pressedKeysNormal:
-            if key in __DICT_KEY_NORMAL.values():
-                pressed.append(list(__DICT_KEY_NORMAL.keys())[list(__DICT_KEY_NORMAL.values()).index(key)])
+            if key in self.__DICT_KEY_NORMAL.values():
+                pressed.append(list(self.__DICT_KEY_NORMAL.keys())[list(self.__DICT_KEY_NORMAL.values()).index(key)])
             else:
                 pressed.append(hex(key))
         return pressed
     def getPressedKeyCont(self):
         pressed=[]
-        for i in range(self.__pressedKeysCont):
+        for i in range(self.__pressedKeysCont.__len__()):
             if self.__pressedKeysCont[i]:
-                pressed.append(__NAMES_KEY_CONTROL[i])
+                pressed.append(self.__NAMES_KEY_CONTROL[i])
         return pressed
     def getPressedKeyMedia(self):
         pressed=[]
-        for i in range(self.__pressedKeysMedia):
-            for j in range(self.__pressedKeysMedia[i]):
+        for i in range(self.__pressedKeysMedia.__len__()):
+            for j in range(self.__pressedKeysMedia[i].__len__()):
                 if self.__pressedKeysMedia[i][j]:
-                    pressed.append(__NAMES_KEY_MEDIA[i][j])
+                    pressed.append(self.__NAMES_KEY_MEDIA[i][j])
         return pressed
     def getPressedKeyAll(self):
         return self.getPressedKeyNormal(),self.getPressedKeyCont(),self.getPressedKeyMedia()
